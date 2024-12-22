06d44007d47e7ed379fc9271974036144829f496
Fauzauza <148870402+Fauzauza@users.noreply.github.com>
2024-12-22 19:26:26 +0800

    complain_view.dart

--- lib/app/modules/home/views/complain_view.dart
+++ lib/app/modules/home/views/complain_view.dart
@@ -10,34 +10,31 @@ class ComplaintPage extends StatefulWidget {
 
 class _ComplaintPageState extends State<ComplaintPage> {
   final ImagePicker _picker = ImagePicker();
-  File? _image; // To store selected image
-  File? _video; // To store selected video
-  VideoPlayerController?
-      _videoController; // To control video playback (optional)
+  final TextEditingController _complaintTextController = TextEditingController(); // Controller untuk teks keluhan
+  File? _image; // Untuk menyimpan gambar yang dipilih
+  File? _video; // Untuk menyimpan video yang dipilih
+  VideoPlayerController? _videoController; // Untuk mengontrol pemutaran video
 
-  // Function to pick an image from camera or gallery
+  // Fungsi untuk memilih gambar dari kamera atau galeri
   Future<void> _pickImage(ImageSource source) async {
-    final XFile? pickedFile =
-        await _picker.pickImage(source: source); // Camera or gallery option
+    final XFile? pickedFile = await _picker.pickImage(source: source);
     if (pickedFile != null) {
       setState(() {
         _image = File(pickedFile.path);
-        _video = null; // Clear the selected video if an image is chosen
-        _videoController
-            ?.dispose(); // Dispose the video controller if video was chosen previously
+        _video = null; // Hapus video jika gambar dipilih
+        _videoController?.dispose();
         _videoController = null;
       });
     }
   }
 
-  // Function to pick a video from camera or gallery
+  // Fungsi untuk memilih video dari kamera atau galeri
   Future<void> _pickVideo(ImageSource source) async {
-    final XFile? pickedFile =
-        await _picker.pickVideo(source: source); // Camera or gallery option
+    final XFile? pickedFile = await _picker.pickVideo(source: source);
     if (pickedFile != null) {
       setState(() {
         _video = File(pickedFile.path);
-        _image = null; // Clear the selected image if a video is chosen
+        _image = null; // Hapus gambar jika video dipilih
         _videoController = VideoPlayerController.file(_video!)
           ..initialize().then((_) {
             setState(() {});
@@ -47,33 +44,34 @@ class _ComplaintPageState extends State<ComplaintPage> {
     }
   }
 
-  // Function to submit the complaint with image/video
+  // Fungsi untuk mengirimkan keluhan dengan gambar/video dan teks
   void _submitComplaint() {
-    if (_image != null || _video != null) {
-      // You can add API call here to submit the complaint
+    final complaintText = _complaintTextController.text; // Ambil teks keluhan
+    if ((complaintText.isNotEmpty && (_image != null || _video != null))) {
+      // Tambahkan logika API atau pengolahan data di sini
 
-      // Show confirmation snackbar
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text("Complaint Submitted!")));
+      // Tampilkan notifikasi konfirmasi
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Complaint Submitted: $complaintText")));
 
-      // Clear the selected image or video after submission
+      // Reset data setelah keluhan dikirim
       setState(() {
         _image = null;
         _video = null;
-        _videoController
-            ?.dispose(); // Dispose video controller if video was selected
+        _videoController?.dispose();
         _videoController = null;
+        _complaintTextController.clear(); // Hapus teks keluhan
       });
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Please add an image or video")));
+          SnackBar(content: Text("Please add an image/video and complaint text")));
     }
   }
 
   @override
   void dispose() {
-    _videoController
-        ?.dispose(); // Clean up video controller when widget is disposed
+    _videoController?.dispose();
+    _complaintTextController.dispose(); // Hapus controller teks
     super.dispose();
   }
 
@@ -84,83 +82,58 @@ class _ComplaintPageState extends State<ComplaintPage> {
         title: Text('Complaint Page'),
       ),
       body: SingleChildScrollView(
-        // Wrap the content in a SingleChildScrollView
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              // Image or video display area
+              // Tampilkan gambar atau video yang dipilih
               if (_image != null) Image.file(_image!),
               if (_video != null)
-                _videoController != null &&
-                        _videoController!.value.isInitialized
+                _videoController != null && _videoController!.value.isInitialized
                     ? AspectRatio(
                         aspectRatio: _videoController!.value.aspectRatio,
                         child: VideoPlayer(_videoController!),
                       )
                     : Container(),
 
-              // Buttons to pick an image or video
+              // Input teks keluhan
+              SizedBox(height: 20),
+              TextField(
+                controller: _complaintTextController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your complaint',
+                  hintText: 'Describe your issue here...',
+                ),
+              ),
+
+              // Tombol untuk memilih gambar atau video
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            10), // Ubah nilai sesuai keinginan
-                      ),
-                    ),
                     onPressed: _image == null
                         ? () => _showImagePickerOptions()
-                        : null, // Disable if image is selected
-                    child: Text(
-                      'Pick Image',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
+                        : null, // Nonaktifkan jika gambar telah dipilih
+                    child: Text('Pick Image'),
                   ),
                   SizedBox(width: 20),
                   ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            10), // Ubah nilai sesuai keinginan
-                      ),
-                    ),
                     onPressed: _video == null
                         ? () => _showVideoPickerOptions()
-                        : null, // Disable if video is selected
-                    child: Text(
-                      'Pick Video',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
+                        : null, // Nonaktifkan jika video telah dipilih
+                    child: Text('Pick Video'),
                   ),
                 ],
               ),
 
-              // Submit button
+              // Tombol kirim keluhan
               SizedBox(height: 20),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        10), // Ubah nilai sesuai keinginan
-                  ),
-                ),
                 onPressed: _submitComplaint,
-                child: Text(
-                  'Submit Complaint',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
+                child: Text('Submit Complaint'),
               ),
             ],
           ),
@@ -169,7 +142,7 @@ class _ComplaintPageState extends State<ComplaintPage> {
     );
   }
 
-  // Function to show options for picking an image (Camera or Gallery)
+  // Fungsi untuk menampilkan dialog pilihan sumber gambar
   void _showImagePickerOptions() async {
     final ImageSource? source = await showDialog<ImageSource>(
       context: context,
@@ -177,15 +150,11 @@ class _ComplaintPageState extends State<ComplaintPage> {
         title: Text('Pick Image Source'),
         actions: [
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(ImageSource.camera);
-            },
+            onPressed: () => Navigator.of(context).pop(ImageSource.camera),
             child: Text('Camera'),
           ),
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(ImageSource.gallery);
-            },
+            onPressed: () => Navigator.of(context).pop(ImageSource.gallery),
             child: Text('Gallery'),
           ),
         ],
@@ -196,7 +165,7 @@ class _ComplaintPageState extends State<ComplaintPage> {
     }
   }
 
-  // Function to show options for picking a video (Camera or Gallery)
+  // Fungsi untuk menampilkan dialog pilihan sumber video
   void _showVideoPickerOptions() async {
     final ImageSource? source = await showDialog<ImageSource>(
       context: context,
@@ -204,15 +173,11 @@ class _ComplaintPageState extends State<ComplaintPage> {
         title: Text('Pick Video Source'),
         actions: [
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(ImageSource.camera);
-            },
+            onPressed: () => Navigator.of(context).pop(ImageSource.camera),
             child: Text('Camera'),
           ),
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(ImageSource.gallery);
-            },
+            onPressed: () => Navigator.of(context).pop(ImageSource.gallery),
             child: Text('Gallery'),
           ),
         ],
